5d8359684a9759dd9114648aba45b410d598295e
hemantd17@gmail.com <hemantd17@gmail.com>
2022-04-22 13:41:03 +0530

    modified runner file

--- src/test/java/runner/testNGTest.java
+++ src/test/java/runner/testNGTest.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="./src/test/resources/features", glue={"stepDefinations", "AppHooks"}, 
 plugin= {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
-public class testNG extends AbstractTestNGCucumberTests
+public class testNGTest extends AbstractTestNGCucumberTests
 {
 	@Override
 	@DataProvider(parallel=true)
